f54b03b7971b453ee214f88fe366e97112d61c93
aaudric <audricgir@gmail.com>
2023-03-15 23:20:09 +0100

    toujours du test

--- site/test.php
+++ site/test.php
@@ -27,29 +27,64 @@ require_once ('jpgraph-4.4.1/src/jpgraph.php');
 require_once ('jpgraph-4.4.1/src/jpgraph_pie.php');
 require_once ('jpgraph-4.4.1/src/jpgraph_pie3d.php');
 
-// Some data
-$data = array(40,60,21,33);
 
-// Create the Pie Graph. 
-$graph = new PieGraph(350,250);
+?>
 
-$theme_class= new VividTheme;
+<?php // content="text/plain; charset=utf-8"
+$datay1 = array(20,15,23,15);
+$datay2 = array(12,9,42,8);
+$datay3 = array(5,17,32,24);
 
+// Setup the graph
+$graph = new Graph(300,250);
+$graph->SetScale("textlin");
 
-// Set A title for the plot
-$graph->title->Set("test");
+$theme_class=new UniversalTheme;
 
-// Create
-$p1 = new PiePlot3D($data);
+$graph->SetTheme($theme_class);
+$graph->img->SetAntiAliasing(false);
+$graph->title->Set('Filled Y-grid');
+$graph->SetBox(false);
+
+$graph->SetMargin(40,20,36,63);
+
+$graph->img->SetAntiAliasing();
+
+$graph->yaxis->HideZeroLabel();
+$graph->yaxis->HideLine(false);
+$graph->yaxis->HideTicks(false,false);
+
+$graph->xgrid->Show();
+$graph->xgrid->SetLineStyle("solid");
+$graph->xaxis->SetTickLabels(array('A','B','C','D'));
+$graph->xgrid->SetColor('#E3E3E3');
+
+// Create the first line
+$p1 = new LinePlot($datay1);
 $graph->Add($p1);
+$p1->SetColor("#6495ED");
+$p1->SetLegend('Line 1');
 
-$p1->SetTheme("water");
-$p1->ShowBorder();
-$p1->SetColor('black');
-$p1->ExplodeAll();
+// Create the second line
+$p2 = new LinePlot($datay2);
+$graph->Add($p2);
+$p2->SetColor("#B22222");
+$p2->SetLegend('Line 2');
+
+// Create the third line
+$p3 = new LinePlot($datay3);
+$graph->Add($p3);
+$p3->SetColor("#FF1493");
+$p3->SetLegend('Line 3');
+
+$graph->legend->SetFrameWeight(1);
+
+// Output line
 $graph->Stroke();
 
 ?>
+
+
 
 
 
