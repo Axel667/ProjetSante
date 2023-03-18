b03891f54f9b2f78e47a969caf0a8627cba3dabf
aaudric <audricgir@gmail.com>
2023-03-18 22:04:47 +0100

    création de la page nos articles qui affichera le titre de nos artciles et permettra de rediriger vers article.php qui permait la lecture de l'artcile en fonction de son id article

---
+++ site/article.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Article</title>
+
+    <?php include("baseD.php"); ?>
+
+    <link rel="stylesheet" type="text/css" href="site.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+</head>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="index.php"><h2>SantÉconomie</h2></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <ul class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="carte.html">Carte</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="comparaison2.php">Comparer</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="nos-article.php">Nos Articles</a>
+              </li>
+              <?php 
+              session_start();
+
+              if (!isset($_SESSION['client'])){
+              }
+              else{
+              echo '<li class="nav-item">
+                <a class="nav-link active" href="create.php">Créer</a>
+              </li>';
+            }?>
+            </ul>
+            <ul class="navbar-nav ms-auto"></ul>
+             <form class="d-flex" method="GET" action="recherche.php">
+                <input class="form-control me-2" name="s" id = "search" type="search" placeholder="Search">
+                <input type="submit" name="search" id = "envoyer" value= "search">
+              </form>
+              <?php
+
+              if (!isset($_SESSION['client'])){
+              echo '<li class="nav-item">
+                <a class="nav-link" href="sign-in.php"><img src="img/user@2x.png" style="height: 30px; width: 30px;"></a>
+              </li>';
+              }
+              else{
+                echo '<li class="nav-item">
+                <a class="nav-link active" href="profil.php"><img src="img/user@2x.png" style="height: 30px; width: 30px;"></a></li>';
+                
+                echo '<li class="nav-item"><a class="nav-link active" id = "logout" href="deconnexion.php">Log out</a></li>';
+                
+              }
+              ?>
+              
+            </ul> 
+            </div>
+            </div>
+            </nav>
+
+<body>
+
+    <?php
+
+        //récupération du paramètre
+        $aa = $_GET['id_article'];
+
+        //connection avec la bdd avec la fonction getBD
+        $bdd = getBD();
+
+        //requete sur la bd avec le paramètre récupéré
+        $rep = $bdd->query("select * from sourcer where id_article= $aa ");
+
+        while ($ligne = $rep ->fetch()){
+
+            echo "<h1>".$ligne['Titre']."</h1>";
+
+            echo $ligne['article'];
+        }
+
+        $rep ->closeCursor();
+    ?>
+    
+</body>
+</html>
---
+++ site/nos-article.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nos Articles</title>
+    <?php include("baseD.php"); ?>
+    <link rel="stylesheet" type="text/css" href="site.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+</head>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="index.php"><h2>SantÉconomie</h2></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <ul class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="carte.html">Carte</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="comparaison2.php">Comparer</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="nos-article.php">Nos Articles</a>
+              </li>
+              <?php 
+              session_start();
+
+              if (!isset($_SESSION['client'])){
+              }
+              else{
+              echo '<li class="nav-item">
+                <a class="nav-link active" href="create.php">Créer</a>
+              </li>';
+            }?>
+            </ul>
+            <ul class="navbar-nav ms-auto"></ul>
+             <form class="d-flex" method="GET" action="recherche.php">
+                <input class="form-control me-2" name="s" id = "search" type="search" placeholder="Search">
+                <input type="submit" name="search" id = "envoyer" value= "search">
+              </form>
+              <?php
+
+              if (!isset($_SESSION['client'])){
+              echo '<li class="nav-item">
+                <a class="nav-link" href="sign-in.php"><img src="img/user@2x.png" style="height: 30px; width: 30px;"></a>
+              </li>';
+              }
+              else{
+                echo '<li class="nav-item">
+                <a class="nav-link active" href="profil.php"><img src="img/user@2x.png" style="height: 30px; width: 30px;"></a></li>';
+                
+                echo '<li class="nav-item"><a class="nav-link active" id = "logout" href="deconnexion.php">Log out</a></li>';
+                
+              }
+              ?>
+              
+            </ul> 
+            </div>
+            </div>
+            </nav>
+<body>
+
+    <a href="index.php" class="btn btn-secondary">Back Home</a>
+
+    <br/>
+
+    <h2>Nos Articles : </h2>
+
+    <?php
+
+        $bdd =  getBD();
+			
+		//requête sur la bd
+		$rep = $bdd->query('select id_article,titre from sourcer');
+
+		while ($ligne = $rep ->fetch()) {
+
+			//création d'un lien contenant le paramètre de l'article en question sur le nom de l'article
+			echo "<a href ='article.php?id_article=".$ligne['id_article']."'><em><strong>" .$ligne['titre']. "</em></strong></a>";
+        }
+
+        $rep ->closeCursor(); 
+    ?>
+				
+    
+</body>
+</html>
