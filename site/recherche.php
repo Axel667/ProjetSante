e51bba961d2b12d4d1244d8d582c3f562500e579
aaudric <audricgir@gmail.com>
2023-03-18 20:01:44 +0100

    ajout de navbar

--- site/recherche.php
+++ site/recherche.php
@@ -9,16 +9,69 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
 </head>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="index.php"><h2>SantÉconomie</h2></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <ul class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="carte.html">Carte</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="comparaison2.php">Comparer</a>
+              </li>
+              <?php 
+              session_start();
+
+              if (!isset($_SESSION['client'])){
+              }
+              else{
+              echo '<li class="nav-item">
+                <a class="nav-link active" href="create.php">Créer</a>
+              </li>';
+            }?>
+            </ul>
+            <ul class="navbar-nav ms-auto"></ul>
+             <form class="d-flex" method="GET" action="recherche.php">
+                <input class="form-control me-2" name="s" id = "search" type="search" placeholder="Search">
+                <input type="submit" name="search" id = "envoyer" value= "search">
+              </form>
+              <?php
+
+              if (!isset($_SESSION['client'])){
+              echo '<li class="nav-item">
+                <a class="nav-link" href="sign-in.php"><img src="img/user@2x.png" style="height: 30px; width: 30px;"></a>
+              </li>';
+              }
+              else{
+                echo '<li class="nav-item">
+                <a class="nav-link active" href="profil.php"><img src="img/user@2x.png" style="height: 30px; width: 30px;"></a></li>';
+                
+                echo '<li class="nav-item"><a class="nav-link active" id = "logout" href="deconnexion.php">Log out</a></li>';
+                
+              }
+              ?>
+              
+            </ul> 
+            </div>
+            </div>
+            </nav>
 
 <body>
 
     <a href="index.php" class="btn btn-secondary">Back Home</a>
 
+    <br/>
+    <br/>
+    
     <?php
 
-        include("baseD.php");
+        echo "<p>Voici les résultats des données pour la recherche : <strong>'".$_GET['s']."'</strong></p>";
 
-        session_start();
+        include("baseD.php");
 
         $bdd = getBD();
         
