9a4cfe565a49677f7300b578647dde51265a3fb3
pacnalfi <arisoy10@mail.ru>
2023-03-19 16:21:53 +0100

    bd sur amazom

--- site/baseD.php
+++ site/baseD.php
@@ -1,9 +1,18 @@
 <?php
 
-    function getBD(){
-        
-    $bdd = new PDO('mysql:host=localhost;dbname=santeco;charset=utf8',
-    'root', '');
+function getBD() {
+    $host = 'database-1.ckmwomwn8pbh.eu-west-3.rds.amazonaws.com';
+    $dbname = 'database-1';
+    $username = 'admin';
+    $password = 'santeconomie';
+
+    $dsn = "mysql:host={$host};dbname={$dbname};charset=utf8";
+
+    try {
+        $bdd = new PDO($dsn, $username, $password);
+    } catch (PDOException $e) {
+        die("Connection failed: " . $e->getMessage());
+    }
 
     return $bdd;
 }
